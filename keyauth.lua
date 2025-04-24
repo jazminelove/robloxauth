8d30ef6ec693c3c823ebdd4a17a9dbbf2b239581
jazminelove <may23rdjp@gmail.com>
2025-04-23 21:24:33 -0500

    Update keyauth.lua

--- keyauth.lua
+++ keyauth.lua
@@ -1,15 +1,16 @@
--- keyauth script (keyauth.lua)
+-- keyauth.lua
 
--- List of valid keys and corresponding HWIDs
 local validKeys = {
-    zjxfmcsl = "02E9DB8F-8551-4X6A-9380-4BFF82B309A0",  -- example HWID, replace with real one
-    cyvvdxhn = "02E9DB8F-8551-4F6A-9380-4BFF82B309A0",
+    zjxfmcsl = "02E9DB8F-8551-4B6A-9380-4BFF82B309A0",
+    cyvvdxhn = "1a2b-3c4d-5e6f",
     wxryxmkl = "abc-123-def-456",
     vbyzrlhf = "xyz-987-pqr-654",
 }
 
--- Function to validate key and HWID
 function validateKey(playerKey, hwid)
-    local validHWID = validKeys[playerKey]
-    return validHWID and hwid == validHWID
+    local requiredHWID = validKeys[playerKey]
+    if requiredHWID then
+        return hwid == requiredHWID
+    end
+    return false
 end
