90c3529d028a93f17efcc776671ce201a7e23cf5
jazminelove <may23rdjp@gmail.com>
2025-04-23 21:21:01 -0500

    Update keyauth.lua

--- keyauth.lua
+++ keyauth.lua
@@ -2,7 +2,7 @@
 
 -- List of valid keys and corresponding HWIDs
 local validKeys = {
-    zjxfmcsl = "02E9DB8F-8551-4B6A-9380-4BFF82B309A0",  -- example HWID, replace with real one
+    zjxfmcsl = "02E9DB8F-8551-4X6A-9380-4BFF82B309A0",  -- example HWID, replace with real one
     cyvvdxhn = "02E9DB8F-8551-4F6A-9380-4BFF82B309A0",
     wxryxmkl = "abc-123-def-456",
     vbyzrlhf = "xyz-987-pqr-654",
