605af4db8d8808e518e4594e942e7a3a8f9df7d1
jazminelove <may23rdjp@gmail.com>
2025-05-04 18:35:39 -0500

    Update keyauth.lua

--- keyauth.lua
+++ keyauth.lua
@@ -1,7 +1,7 @@
 -- keyauth.lua
 
 local validKeys = {
-    jazmine = "02d36cb2-8db4-4292-bf07-838c1d943dbf",
+    jazmine = "02E9DB8F-8551-4B6A-9380-4BFF82B309A0",
     cyvvdxhn = "1a2b-3c4d-5e6f",
     wxryxmkl = "abc-123-def-456",
     vbyzrlhf = "xyz-987-pqr-654",
