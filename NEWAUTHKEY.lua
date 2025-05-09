14ba377ee08560c8d7e945579800a12a798cccac
jazminelove <may23rdjp@gmail.com>
2025-05-09 17:55:58 -0500

    Update NEWAUTHKEY.lua

--- NEWAUTHKEY.lua
+++ NEWAUTHKEY.lua
@@ -1,16 +1,6 @@
 local validKeys = {
     "jazmine",
-    "cyvvdxhn",
-    "bf3483asr9ns",
-    "dw3ager4as08",
-    "4e59jse76sdf",
-    "d2da4swe0sdf",
-    "f32bao34uin2",
-    "896fb7drntom",
-    "vnxa34r87ujf",
-    "r23jap9seu8d",
-    "350ws78erhsr",
-    "3srdbs0dir33",
+    "cyvvdxhn"
 }
 
 function validateKey(key)
