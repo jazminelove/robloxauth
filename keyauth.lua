760c7a7f4249ee384d73a97c5c617e052a0cf8e7
jazminelove <may23rdjp@gmail.com>
2025-05-10 23:40:06 -0500

    Update keyauth.lua

--- keyauth.lua
+++ keyauth.lua
@@ -1,10 +1,10 @@
 -- KEYS
 local validKeys = {
-    jazmine = "02E9DB8F-8551-4B6A-9380-4BFF82B309A0",
-    cyvvdxhn = "C43DD5B6-23AB-4B3A-86FD-5A36319471E2", -- oulyo guy
-    wxryxmkl = "abc-123-def-456",
-    vbyzrlhf = "xyz-987-pqr-654",
-    bruhwtf = "02E9DB8F-8551-4B6A-9380-4BFF82B309A0", -- allows access regardless of HWID
+    jazmine = "N/A",
+    cyvvdxhn = "N/A", -- oulyo guy
+    wxryxmkl = "N/A",
+    vbyzrlhf = "N/A",
+    bruhwtf = "N/A", -- allows access regardless of HWID
 }
 
 function validateKey(playerKey, hwid)
