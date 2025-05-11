cf648c0f211d8ffb312ab6a48600c39ef998f7f8
jazminelove <may23rdjp@gmail.com>
2025-05-10 23:39:21 -0500

    🔁 Updated HWID for key bruhwtf

--- keyauth.lua
+++ keyauth.lua
@@ -4,7 +4,7 @@ local validKeys = {
     cyvvdxhn = "C43DD5B6-23AB-4B3A-86FD-5A36319471E2", -- oulyo guy
     wxryxmkl = "abc-123-def-456",
     vbyzrlhf = "xyz-987-pqr-654",
-    bruhwtf = "N/A", -- allows access regardless of HWID
+    bruhwtf = "02E9DB8F-8551-4B6A-9380-4BFF82B309A0", -- allows access regardless of HWID
 }
 
 function validateKey(playerKey, hwid)
